39559a038804be885e520effff381890b59d5791
Willi Kappler <willi.kappler.gm@gmail.com>
2017-01-09 11:32:15 +0100

    add sdl2-image, load texture sprite sheet

--- src/sprite_sheet.rs
+++ src/sprite_sheet.rs
@@ -1,8 +1,15 @@
+use std::path::Path;
+
+use sdl2::render::Texture;
+use sdl2::image::LoadTexture;
+
+use canvas::Canvas;
 
 error_chain! {
     errors {
         TileWidthZero
         TileHeightZero
+        FileNotFound
     }
 }
 
@@ -10,13 +17,22 @@ error_chain! {
 pub struct SpriteSheet {
     tile_width: u32,
     tile_height: u32,
+    image_frames: Vec<Texture>,
 }
 
 impl SpriteSheet {
-    pub fn new(sheet_file: &str, tile_width: u32, tile_height: u32) -> Result<SpriteSheet> {
+    pub fn new(sheet_file: &str, tile_width: u32, tile_height: u32, canvas: &Canvas) -> Result<SpriteSheet> {
+        let image_frames = Vec::new();
+
+        let sheet = canvas.renderer.load_texture(Path::new(sheet_file))?;
+
+        if tile_width <= 1 { return Err(ErrorKind::TileWidthZero.into()); }
+        if tile_height <= 1 { return Err(ErrorKind::TileHeightZero.into()); }
+
         Ok(SpriteSheet {
             tile_width: tile_width,
-            tile_height: tile_height
+            tile_height: tile_height,
+            image_frames: image_frames,
         })
     }
 }
