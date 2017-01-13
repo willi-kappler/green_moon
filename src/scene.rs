3394308a1572872727ccd8cfdd93c760c151ca80
Willi Kappler <willi.kappler@uni-tuebingen.de>
2017-01-13 09:29:01 +0100

    add scene message

--- src/scene.rs
+++ src/scene.rs
@@ -2,12 +2,18 @@
 use game::GameObjects;
 use resource_manager::ResourceManager;
 
+pub enum SceneMessage {
+    Continue,
+    ChangeScene(usize),
+    QuitGame,
+}
+
 pub trait Scene {
     fn enter(&mut self, resource_manager: &ResourceManager, game_objects: &mut GameObjects) {}
 
     fn leave(&mut self, resource_manager: &ResourceManager, game_objects: &mut GameObjects) {}
 
-    fn update(&mut self, resource_manager: &ResourceManager, game_objects: &mut GameObjects) -> bool { false }
+    fn update(&mut self, resource_manager: &ResourceManager, game_objects: &mut GameObjects) -> SceneMessage { SceneMessage::QuitGame }
 
     fn draw(&mut self, resource_manager: &ResourceManager, game_objects: &mut GameObjects) {}
 }
@@ -30,18 +36,26 @@ impl SceneManager {
     }
 
     pub fn run(mut self, resource_manager: ResourceManager, mut game_objects: GameObjects) {
-        let ref mut current_scene = self.scenes[self.current_scene];
+        self.scenes[self.current_scene].enter(&resource_manager, &mut game_objects);
 
-        current_scene.enter(&resource_manager, &mut game_objects);
-
-        let mut quit = current_scene.update(&resource_manager, &mut game_objects);
-        current_scene.draw(&resource_manager, &mut game_objects);
-
-        while !quit {
-            quit = current_scene.update(&resource_manager, &mut game_objects);
+        loop {
+            let ref mut current_scene = self.scenes[self.current_scene];
+            let message = current_scene.update(&resource_manager, &mut game_objects);
             current_scene.draw(&resource_manager, &mut game_objects);
-        }
 
-        current_scene.leave(&resource_manager, &mut game_objects);
+            match message {
+                SceneMessage::QuitGame => {
+                    current_scene.leave(&resource_manager, &mut game_objects);
+                    break
+                },
+                SceneMessage::ChangeScene(new_scene) => {
+                    current_scene.leave(&resource_manager, &mut game_objects);
+                    self.current_scene = new_scene;
+                },
+                _ => {
+                    // Continue the loop, nothing to do here
+                }
+            }
+        }
     }
 }
