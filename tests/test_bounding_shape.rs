9b0f30543dac12d22081bf32e5d3fad69ce0d5aa
Willi Kappler <willi-kappler@users.noreply.github.com>
2017-01-16 22:16:14 +0100

    more bounding shape tests

--- tests/test_bounding_shape.rs
+++ tests/test_bounding_shape.rs
@@ -6,11 +6,67 @@ use green_moon::{BoundingShape, collides};
 
 
 #[test]
-fn test_box_box() {
+fn test_box_box01() {
     let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: 5.0, y: 5.0 }, width: 3.0, height: 2.0 };
     let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: 5.0, y: 4.0 }, width: 1.0, height: 1.0 };
 
     assert_eq!(collides(b1, b2), Some(Vector2D{ x: 0.0, y: -1.0 }));
+}
+
+#[test]
+fn test_box_box02() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: 5.0, y: 4.0 }, width: 1.0, height: 1.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: 5.0, y: 5.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), Some(Vector2D{ x: 0.0, y: 1.0 }));
+}
+
+#[test]
+fn test_box_box03() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: 5.0, y: 3.0 }, width: 1.0, height: 1.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: 5.0, y: 5.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), None);
+}
+
+#[test]
+fn test_box_box04() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: -2.0, y: -2.0 }, width: 4.0, height: 4.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: 2.0, y: 2.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), Some(Vector2D{ x: 4.0, y: 4.0 }));
+}
+
+#[test]
+fn test_box_box05() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: -2.0, y: -2.0 }, width: 4.0, height: 4.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: -3.0, y: 2.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), Some(Vector2D{ x: -1.0, y: 4.0 }));
+}
+
+#[test]
+fn test_box_box06() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: -2.0, y: -2.0 }, width: 4.0, height: 4.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: -3.0, y: -3.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), Some(Vector2D{ x: -1.0, y: -1.0 }));
+}
+
+#[test]
+fn test_box_box07() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: -2.0, y: -2.0 }, width: 4.0, height: 4.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: 2.0, y: -3.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), Some(Vector2D{ x: 4.0, y: -1.0 }));
+}
+
+#[test]
+fn test_box_box08() {
+    let b1 = BoundingShape::Rectangle{ position: Vector2D{ x: -2.0, y: -2.0 }, width: 4.0, height: 4.0 };
+    let b2 = BoundingShape::Rectangle{ position: Vector2D{ x: -1.0, y: -1.0 }, width: 1.0, height: 1.0 };
+
+    assert_eq!(collides(b1, b2), Some(Vector2D{ x: 1.0, y: 1.0 }));
 }
 
 #[test]
